539ca3e357a74ffc9f0ef70c79eb368476788fda
Ruslan <ruslan.salmin@gmail.com>
2024-01-26 13:45:34 +0300

    Modul 2 Main operat

---
+++ Mod2Func.py
@@ -0,0 +1,11 @@
+def print_params(param):
+    print(param, param)
+
+i = 0
+while i < 5:
+    i = i+1
+    vvod_param = input('введите число ')
+    print_params(vvod_param)
+else:
+    print('ввод закончен')
+
