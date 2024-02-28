eb00a55faa3e831a57a35649b3660fdadd8e85f1
Ruslan <ruslan.salmin@gmail.com>
2024-02-28 17:02:36 +0300

    Modul 6

--- Modul_6/Mod6_heir_class.py
+++ Modul_6/Mod6_heir_class.py
@@ -7,17 +7,17 @@ class Car:
             self.__class__.__name__, self.price, self.horse_powers())
 
 class Nissan(Car):
+    price = 900000
     def horse_powers(self):
         return 120
 
 class Kia(Car):
+    price = 1100000
     def horse_powers(self):
         return 180
 
 my_car = Nissan()
-my_car.price = 900000
 print(my_car)
 
 my_car = Kia()
-my_car.price = 1100000
 print(my_car)
