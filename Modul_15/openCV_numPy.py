5945294342104a48c5bd72890a5193016f40899b
Ruslan <ruslan.salmin@gmail.com>
2024-06-04 14:31:37 +0300

    Modul 15

--- Modul_15/openCV_numPy.py
+++ Modul_15/openCV_numPy.py
@@ -9,8 +9,8 @@
 import numpy as np
 import cv2
 
-screen_width = 600
-screen_height = 600
+screen_width = 800
+screen_height = 800
 
 def get_gradient(width, height, start_list, stop_list, is_horizontal_list):
     result = np.zeros((height, width, len(start_list)), dtype='uint8')
@@ -21,22 +21,24 @@ def get_gradient(width, height, start_list, stop_list, is_horizontal_list):
             result[:, :, i] = np.tile(np.linspace(start, stop, height), (width, 1)).T
     return result
 
+screen = get_gradient(screen_width, screen_height, (0, 0, 0), (255, 255, 255), (True, True, True))
+# screen = get_gradient(screen_width, screen_height, (0, 0, 0), (255, 255, 255), (False, False, False))
+# screen = get_gradient(screen_width, screen_height, (0, 0, 0), (255, 255, 255), (True, False, False))
 
+center_x = screen.shape[1]//2-100
+center_y = screen.shape[0]//2-100
+cv2.circle(screen, (center_x, center_y), 250, (255,255,255), thickness=3)
+# cv2.circle(screen, (center_x, center_y-25), 125, (255,255,255), thickness=3)
 
-screen = get_gradient(screen_width, screen_height, (0, 0, 192), (255, 255, 64), (True, False, False))
+cv2.ellipse(screen, (center_x, center_y), (100,100), 0, 180, 0, (0,255,0), thickness=3)
+cv2.ellipse(screen, (center_x, center_y), (50,50), 0, 180, 0, (0,255,0), thickness=3)
 
-cv2.circle(screen, (screen.shape[1]//2, screen.shape[0]//2), 250, (255,255,255), thickness=3)
-# cv2.circle(screen, (screen.shape[1]//2, screen.shape[0]//2-25), 125, (255,255,255), thickness=3)
-
-cv2.ellipse(screen, (screen.shape[1]//2, screen.shape[0]//2), (100,100), 0, 180, 0, (0,255,0), thickness=3)
-cv2.ellipse(screen, (screen.shape[1]//2, screen.shape[0]//2), (50,50), 0, 180, 0, (0,255,0), thickness=3)
-
-cv2.line(screen, (screen.shape[1]//2+50, screen.shape[0]//2), (screen.shape[1]//2+50, screen.shape[0]//2-100), (255,0,0), thickness=3)
-cv2.line(screen, (screen.shape[1]//2-50, screen.shape[0]//2), (screen.shape[1]//2-50, screen.shape[0]//2-100), (255,0,0), thickness=3)
-cv2.line(screen, (screen.shape[1]//2+100, screen.shape[0]//2), (screen.shape[1]//2+100, screen.shape[0]//2-100), (255,0,0), thickness=3)
-cv2.line(screen, (screen.shape[1]//2-100, screen.shape[0]//2), (screen.shape[1]//2-100, screen.shape[0]//2-100), (255,0,0), thickness=3)
-cv2.line(screen, (screen.shape[1]//2-50, screen.shape[0]//2-100), (screen.shape[1]//2-100, screen.shape[0]//2-100), (0,0,255), thickness=3)
-cv2.line(screen, (screen.shape[1]//2+50, screen.shape[0]//2-100), (screen.shape[1]//2+100, screen.shape[0]//2-100), (0,0,255), thickness=3)
+cv2.line(screen, (center_x+50, center_y), (center_x+50, center_y-100), (255,0,0), thickness=3)
+cv2.line(screen, (center_x-50, center_y), (center_x-50, center_y-100), (255,0,0), thickness=3)
+cv2.line(screen, (center_x+100, center_y), (center_x+100, center_y-100), (255,0,0), thickness=3)
+cv2.line(screen, (center_x-100, center_y), (center_x-100, center_y-100), (255,0,0), thickness=3)
+cv2.line(screen, (center_x-50, center_y-100), (center_x-100, center_y-100), (0,0,255), thickness=3)
+cv2.line(screen, (center_x+50, center_y-100), (center_x+100, center_y-100), (0,0,255), thickness=3)
 
 cv2.imshow('logo', screen)
 cv2.waitKey(0)
